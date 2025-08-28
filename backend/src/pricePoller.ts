ccf7a10e3fb46f2bd683ec9274ce96b4517a08f1
AyushRajSinghParihar <1BI22RI009@bit-bangalore.edu.in>
2025-08-29 02:55:37 +0530

    refactor: enhance WebSocket data handling and integrate Redis for message publishing

--- backend/src/pricePoller.ts
+++ backend/src/pricePoller.ts
@@ -1,68 +1,96 @@
 import WebSocket = require("ws");
 import pg = require("pg");
+import redis = require("redis");
 
-const ws = new WebSocket("wss://stream.binance.com:9443/stream?streams=btcusdt@trade");
-const client = new pg.Client({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    password: "password",
-    database: "postgres"
-})
+const publisher = redis.createClient();
 
-client.connect();
+await publisher.connect();
+
+const ws = new WebSocket(
+  "wss://stream.binance.com:9443/stream?streams=btcusdt@trade",
+);
+const pool = new pg.Pool({
+  host: "localhost",
+  port: 5432,
+  user: "postgres",
+  password: "password",
+  database: "postgres",
+});
+
+await pool.connect();
+
+let buffer: any[] = [];
+const BATCH_SIZE = 500;
+const FLUSH_INTERVAL = 500;
+async function flushBuffer() {
+  if (buffer.length === 0) return;
+  const batch = buffer;
+  buffer = [];
+
+  const values: any[] = [];
+  const placeholders: string[] = [];
+
+  batch.forEach((trade, i) => {
+    const baseIndex = i * 9;
+    placeholders.push(`($${baseIndex + 1}, $${baseIndex + 2}, $${baseIndex + 3}, 
+                                $${baseIndex + 4},$${baseIndex + 5},$${baseIndex + 6},
+                                $${baseIndex + 7},$${baseIndex + 8},$${baseIndex + 9},)`);
+    values.push(
+      trade.e,
+      trade.e,
+      trade.E,
+      trade.s,
+      Number(trade.a),
+      Number(trade.p),
+      Number(trade.q),
+      trade.T,
+      trade.m,
+      new Date(trade.E),
+    );
+  });
+  const query = `INSERT INTO master_agg_trade(
+            event_type, 
+            event_time, 
+            symbol, 
+            aggregate_trade_id, 
+            price,
+            quantity, 
+            trade_time, 
+            market_maker,
+            time
+        ) VALUES ${placeholders.join(",")}`;
+
+  try {
+    await pool.query(query, values);
+    console.log(`Inserted ${batch.length} rows`);
+  } catch (err) {
+    console.error(`Error received while inserting batch: ${err}`);
+  }
+}
+
+setInterval(flushBuffer, FLUSH_INTERVAL);
 
 // Websocket logic
-ws.on('open', ()=> {
-    console.log('WebSocket connection established');
-})
+ws.on("open", () => {
+  console.log("WebSocket connection established");
+});
 
-ws.on('error', ()=> {
-    console.log('Error establishing the websocket connection');
-})
+ws.on("error", () => {
+  console.log("Error establishing the websocket connection");
+});
 
-// TODO : add batch processing to make it production ready
+ws.on("message", (data) => {
+  const parsedData = JSON.parse(data.toString());
+  // Handle the parsed data
+  const trade = parsedData.data;
 
-ws.on('message', (data)=> {
-    const parsedData = JSON.parse(data.toString());
-    // Handle the parsed data
-    const streamData = parsedData.data;
-    const insertData = `INSERT INTO master_agg_trade(
-        event_type, 
-        event_time, 
-        symbol, 
-        aggregate_trade_id, 
-        price, 
-        quantity, 
-        trade_time, 
-        market_maker,
-        time
-    ) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+  buffer.push(trade);
+  if (buffer.length >= BATCH_SIZE) {
+    flushBuffer();
+  }
+  await publisher.publish("btcusdt:trade", JSON.stringify(trade));
+});
 
-    const timeObject = new Date(streamData.E);
-
-    const values = [
-        streamData.e,
-        streamData.E,
-        streamData.s,
-        Number(streamData.a),
-        Number(streamData.p),
-        Number(streamData.q),
-        streamData.T,
-        streamData.m,
-        timeObject
-    ]
-    async function dataIngestion() {
-    try{
-        await client.query(insertData, values);
-    }
-    catch (err) {
-        console.error('Error inserting data: ', err);
-    }
-}
-    dataIngestion();
-})
-
-ws.on('close', ()=> {
-    console.log('WebSocket connection closed');
-})
+ws.on("close", () => {
+  console.log("WebSocket connection closed");
+});
